7ae8331e3f9201fd10ec670e0a6d1f27e57674f9
LaDorada <masasladorada@gmail.com>
2022-06-05 15:06:12 -0500

    Crear un index por fuera

---
+++ web/Index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html lang="ES" >
+     <head>
+         <meta charset="utf-8"/>
+         <title>INICIO</title>
+       
+         <meta name="description" content="Fabrica de tapas de empanadas y Pascualinas"/>
+         <meta name="author" content="Rosamrl"/>
+         <meta name="keywords" content="empanadas, tapas, pascualinas, dorada, crocante"/>
+     
+         <link rel="shortcut icon" href="Vistas/Images/Empanada feliz.png" type="image/x-icon">
+         <LINK REL=StyleSheet HREF="Vistas/CSS/Master.css" TYPE="text/css" MEDIA=screen>
+     </head>
+     
+      <body>
+        <header>
+            <h1>La Dorada</h1>  <!--<img src="Vistas/Images/logofeliz.png" align ="right" width = "55" height = "55"/> -->
+
+          <nav class="navegacion">
+            <ul class="menu"> 
+                <li><a href="Vistas/IniciarSesion.php">Iniciar Sesión</a></li>
+                <li><a href="Vistas/Index.php">Inicio</a></li>
+                <li><a href="Vistas/Nosotros.php">Nosotros</a></li>
+                <li><a href="Vistas/Productos.php">Productos</a></li>
+              
+                <li><a href="Vistas/Contactodist.php">Distribuidores</a></li>
+                <li><a href="Vistas/Contacto.php">Contacto</a></li>
+                <li><a href="Vistas/Salir.php">Cerrar Sesión</a></li>
+            
+            </ul>
+          </nav> 
+          <div class="link-icons">
+                    <a href="Vistas/Carrito.php">
+						<i class="fas fa-shopping-cart"></i>
+					</a>
+                </div>
+
+
+
+        </header>
+       
+       <div class="bienv">/
+        
+                  <h1> Bienvenidos a la Dorada</h1>
+                  
+                      <img style="display:block;margin:0 auto 0 auto;" src="Images/Doradas.jpg" alt="descripción" width="350" height="350" />
+                  
+                 </br>
+
+                  <h3 align ="center" >La masa más rica y crocante del país</h3> </br>
+
+       </div>
+
+        
+       
+    
+    
+    
+
+</html>     
